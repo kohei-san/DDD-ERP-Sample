b275b1d9d244462729599a22be9f8c5888231551
kohei-san <79580640+kohei-san@users.noreply.github.com>
2026-02-08 18:28:33 +0900

    Edit:画面遷移時に破棄済みのインスタンスを参照するエラー対処

--- DDD-ERP Sample/ServiceCollectionExtensionsForm.cs
+++ DDD-ERP Sample/ServiceCollectionExtensionsForm.cs
@@ -14,9 +14,10 @@ namespace DDD_ERP_Sample
             this IServiceCollection services)
         {
             // Formサービスの登録
-            services.AddScoped<MainMenuForm>();
-            services.AddScoped<ItemMenuForm>();
-            services.AddScoped<Forms.Item.ItemMasterRegisterForm>();
+            // Transient: 毎回新しいインスタンスを生成で登録する　→　Scopedの場合、閉じて再度開こうとすると破棄済みのインスタンスを参照しようとして例外が発生する
+            services.AddTransient<MainMenuForm>();
+            services.AddTransient<ItemMenuForm>();
+            services.AddTransient<Forms.Item.ItemMasterRegisterForm>();
             return services;
         }
 
